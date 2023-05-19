748c84371081fbb4ffafc92f0126824694111238
Ashvin Ravi <ravia04@li03c02.chimera.hpc.mssm.edu>
2023-05-19 14:01:44 -0400

    liftover from hg38 to hg19 works now

--- scripts/liftover_script.sh
+++ scripts/liftover_script.sh
@@ -2,18 +2,23 @@
 #liftover bed files to hg19
 
 #ml util-linux
-# ml liftover/09-Jul-2019
+ml liftover/09-Jul-2019
 input_bed=$1
 output_bed=$2 
 genome_build=$3
-hg19="hg19"
+#hg19="hg19"
 echo $genome_build
 
-if [[ "$genome_build"=="${hg19}" ]];
+if [[ "${genome_build}" == "hg19" ]]
 then
    cp $input_bed $output_bed
-else
+elif [[ "$genome_build" == "hg38" ]]
+then
    liftOver $input_bed data/hg38ToHg19.over.chain.gz $output_bed ${output_bed}_unMapped
    rm ${output_bed}_unMapped
-fi 
+   echo hello!
+else 
+   echo "Please specify correct hg build - LDSC cannot support non-hg19 or hg38 files."
+fi
+
 
